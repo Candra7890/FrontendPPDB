9b910c810bdfab867dd458e5bb13ac6d5a7eb460
Candra <gungcandra54@gmail.com>
2024-05-24 21:21:54 +0800

    front

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -435,7 +435,7 @@
                 <div class="row">
 
                 <div class="col-lg-3 col-12 mb-4 pb-2">
-                    <a class="navbar-brand d-flex align-items-center" href="/topiclist/index.html">
+                    <a class="navbar-brand d-flex align-items-center" href="/">
                         <img class="mr-2" src="/topiclist/images/OIP__15_-removebg-preview.png" style="width: 100px; height: 100;">
                         <div>
                             <span>PPDB</span><br>
