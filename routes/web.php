7dd5fe605970c3b9e80149ad08828bfd06005b53
Candra <gungcandra54@gmail.com>
2024-05-28 08:53:15 +0800

    front

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,7 @@ use App\Http\Controllers\HomepageController;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 Route::resource('provinsi', \App\Http\Controllers\MProvinsiController::class)->middleware('auth');
 Route::resource('kabupaten', \App\Http\Controllers\MKabupatenController::class)->middleware('auth');
