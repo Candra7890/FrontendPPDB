adab1363fc818ee394fb6296d15c3a8d2b362a4d
Candra <gungcandra54@gmail.com>
2024-05-28 09:03:10 +0800

    front

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        $url = 'https://tabanan-frontend.ppdb-online.web.id/login';
+        return redirect ('auth.login');
     }
 
     /**
